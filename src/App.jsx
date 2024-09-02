16b2dd35831e1c0ed3396119368b52ce4df3c474
gokul-saaragh <gokulchandan24@gmail.com>
2024-09-02 18:11:28 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Sidebar from "./components/common/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
+import UsersPage from "./pages/UsersPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<OverviewPage />} />
           <Route path="/products" element={<ProductsPage />} />
+          <Route path="/users" element={<UsersPage />} />
         </Routes>
       </div>
     </>
